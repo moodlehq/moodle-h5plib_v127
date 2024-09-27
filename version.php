bd82051e4815400bdda505605e2b86c7f6444d4f
Mikel Martín <mikel@moodle.com>
2024-09-27 13:23:09 +0200

    Change requires version
    Reduce the requires version to 4.2.

--- version.php
+++ version.php
@@ -25,5 +25,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2023100900;       // The current module version (Date: YYYYMMDDXX).
-$plugin->requires  = 2023100400;       // Requires this Moodle version.
+$plugin->requires  = 2023041800;       // Requires this Moodle version.
 $plugin->component = 'h5plib_v127';    // Full name of the plugin (used for diagnostics).
